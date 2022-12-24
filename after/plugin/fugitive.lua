5e77530a0dac33b71c595229bd3e1c4f6dc29235
sulaim <sulaim@pop-os.localdomain>
2022-12-24 16:21:25 +0530

    eddited fugitive a bit

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local Tos_Fugitive = vim.api.nvim_create_augroup("Tos_Fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = ThePrimeagen_Fugitive,
+    group = Tos_Fugitive,
     pattern = "*",
     callback = function()
         if vim.bo.ft ~= "fugitive" then
