aa8d8642a567f87458e379fcdbf782c46a2e5a27
sulaim <sulaim@pop-os.localdomain>
2022-12-25 13:21:10 +0530

    added control s to save

--- lua/tos/remap.lua
+++ lua/tos/remap.lua
@@ -28,12 +28,13 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-s>",":w<CR>")
+vim.keymap.set("i", "<C-s>","<Esc>:w<CR>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set({"n", "i"}, "<C-s>", "<cmd>w<CR>")
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
